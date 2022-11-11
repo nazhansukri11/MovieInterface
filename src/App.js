5d7562b3b0c776e8b4562ae80b79e8b11347a1ae
nazhan github <nazhansukri11@gmail.com>
2022-11-11 20:56:02 +0800

    Done

--- src/App.js
+++ src/App.js
@@ -1,45 +1,59 @@
 import React from 'react';
 import './App.css';
-import SearchIcon from './search.svg'
-import {useEffect} from 'react';
+import SearchIcon from './search.svg';
+import {useState,useEffect} from 'react';
+import MovieCard from './MovieCard';
 
 //create API from omdbapi
 const API_KEY='https://www.omdbapi.com?apikey=26a4b140';
 
 const App=()=>{
 
-    //function to call data
-    const searchMovies=async (title)=>{
-        const response=await fetch(`${API_KEY}&s=${title}`);
-        const data=await response.json();
+    const [movies,setMovies]=useState([]);
+    const [searchTerm,setSearchTerm]=useState('');
 
-        console.log(data.Search);
-    }
-
-    //fetch data from API when load
-    useEffect(()=>{
+     //fetch data from API when load
+     useEffect(()=>{
         searchMovies('spiderman');
     },[]);
 
+    //function to call data
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_KEY}&s=${title}`);
+        const data = await response.json();
+
+       setMovies(data.Search);
+    };
+
     //render
     return(
-        <div className="App">
+        <div className="app">
             <h1>MovieUI</h1>
 
             <div className="search">
                 <input 
                     placeholder="Search for movies"
-                    value="Superman"
-                    onChange={()=>{}}
+                    value={searchTerm}
+                    onChange={(e)=>setSearchTerm(e.target.value)}
                 />
                 <img 
                 src={SearchIcon} 
                 alt="search"
-                onClick={()=>{}}
+                onClick={()=>searchMovies(searchTerm)}
                 />
             </div>
 
-            
+            {movies?.length>0 ? (
+                <div className="container">
+                {movies.map((movie)=>(
+                <MovieCard movie={movie} />
+                ))}
+                </div>
+                ):(
+                    <div className="empty">
+                    <h2>No movies found</h2>
+                    </div>
+                )}
         </div>
     );
 }
