ca30c7161a2c391e18bb06cbbf00fb8b4d9983f6
nazhan github <nazhansukri11@gmail.com>
2022-11-11 18:21:04 +0800

    Add function to fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//create API from omdbapi
+const API_KEY='https://www.omdbapi.com?apikey=26a4b140';
+
+const App=()=>{
+    
+    //function to call data
+    const searchMovies=async (title)=>{
+        const response=await fetch(`${API_KEY}&s=${title}`);
+        const data=await response.json();
+
+        console.log(data.Search);
+    }
+
+    //fetch data from API when load
+    useEffect(()=>{
+        searchMovies('spiderman');
+    },[]);
+
+    //render
+    return(
+        <div className="App">
+            <h1>App</h1>
+        </div>
+    );
 }
 
 export default App;
