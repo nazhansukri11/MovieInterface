58a3ee8db8d30d584f56cac54352b634b8f2a5b9
nazhan github <nazhansukri11@gmail.com>
2022-11-11 19:44:57 +0800

    Add search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './App.css';
+import SearchIcon from './search.svg'
 import {useEffect} from 'react';
 
 //create API from omdbapi
 const API_KEY='https://www.omdbapi.com?apikey=26a4b140';
 
 const App=()=>{
-    
+
     //function to call data
     const searchMovies=async (title)=>{
         const response=await fetch(`${API_KEY}&s=${title}`);
@@ -22,7 +24,22 @@ const App=()=>{
     //render
     return(
         <div className="App">
-            <h1>App</h1>
+            <h1>MovieUI</h1>
+
+            <div className="search">
+                <input 
+                    placeholder="Search for movies"
+                    value="Superman"
+                    onChange={()=>{}}
+                />
+                <img 
+                src={SearchIcon} 
+                alt="search"
+                onClick={()=>{}}
+                />
+            </div>
+
+            
         </div>
     );
 }
